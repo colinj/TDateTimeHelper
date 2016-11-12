486d94ea9744cc04efc79230d2d6448e8830adaa
Colin Johnsun <colin.johnsun@gmail.com>
2016-11-12 11:50:56 +1100

    Unit tests for StartOfXXX and EndOfXXX methods.

--- Tests/TDateTimeHelper.Tests.pas
+++ Tests/TDateTimeHelper.Tests.pas
@@ -32,6 +32,22 @@ type
     procedure DateTime_Second;
     [Test]
     procedure DateTime_Millisecond;
+    [Test]
+    procedure DateTime_StartOfYear;
+    [Test]
+    procedure DateTime_EndOfYear;
+    [Test]
+    procedure DateTime_StartOfMonth;
+    [Test]
+    procedure DateTime_EndOfMonth;
+    [Test]
+    procedure DateTime_StartOfWeek;
+    [Test]
+    procedure DateTime_EndOfWeek;
+    [Test]
+    procedure DateTime_StartOfDay;
+    [Test]
+    procedure DateTime_EndOfDay;
   end;
 
 implementation
@@ -101,6 +117,46 @@ end;
 procedure TDateTimeHelperTestObject.DateTime_Millisecond;
 begin
   Assert.AreEqual(MilliSecondOf(FExpected), FActual.Millisecond);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_StartOfYear;
+begin
+  Assert.AreEqual(StartOfTheYear(FExpected), FActual.StartOfYear);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_EndOfYear;
+begin
+  Assert.AreEqual(EndOfTheYear(FExpected), FActual.EndOfYear);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_StartOfMonth;
+begin
+  Assert.AreEqual(StartOfTheMonth(FExpected), FActual.StartOfMonth);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_EndOfMonth;
+begin
+  Assert.AreEqual(EndOfTheMonth(FExpected), FActual.EndOfMonth);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_StartOfWeek;
+begin
+  Assert.AreEqual(StartOfTheWeek(FExpected), FActual.StartOfWeek);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_EndOfWeek;
+begin
+  Assert.AreEqual(EndOfTheWeek(FExpected), FActual.EndOfWeek);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_StartOfDay;
+begin
+  Assert.AreEqual(StartOfTheDay(FExpected), FActual.StartOfDay);
+end;
+
+procedure TDateTimeHelperTestObject.DateTime_EndOfDay;
+begin
+  Assert.AreEqual(EndOfTheDay(FExpected), FActual.EndOfDay);
 end;
 
 initialization
